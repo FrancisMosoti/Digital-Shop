02bf99a2348fa8745b21ae839a9ba089d39652c7
FrancisMosoti <francismosoti20@gmail.com>
2023-11-08 17:22:48 +0300

    Product Section

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views as my_views
 urlpatterns = [
     path('', my_views.products, name='products'),
     path('add-products/', my_views.add_products, name='add-products'),
+    path('all-products/', my_views.products, name='products'),
+    path('update-products/', my_views.update_products, name='update-products'),
 ]
